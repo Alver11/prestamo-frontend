e68ad53ef2995836fd9e3dd2f5cc9231bd71a1be
tic-pima <alverromero11@gmail.com>
2024-04-11 00:24:53 -0400

    ajustes para el cors

--- src/services/modules/user/account.ts
+++ src/services/modules/user/account.ts
@@ -1,16 +1,13 @@
 import { useFetch } from '/@src/composable/useFetch'
 import { useUserSession } from '/@src/stores/userSession'
-import { useApi } from '/@src/composable/useApi'
 
 export async function authenticateUser(route: string, body: object) {
-  // const $fetch = useFetch()
-  const api = useApi()
+  const $fetch = useFetch()
   const userSession = useUserSession()
 
-  // const data = await $fetch(route,{method: 'POST', body: body })
-  const { data: data } = await api.post(route)
+  const data = await $fetch(route,{method: 'POST', body: body })
   userSession.setToken(data.token)
 
-  // const user = await $fetch('user')
-  // userSession.setUser(user)
+  const user = await $fetch('user')
+  userSession.setUser(user)
 }
