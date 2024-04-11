ef7f34284257a7c9f938cde57f75bb046ba30e29
tic-pima <alverromero11@gmail.com>
2024-04-11 08:38:12 -0400

    ajustes para el cors

--- src/composable/useFetch.ts
+++ src/composable/useFetch.ts
@@ -10,6 +10,7 @@ export function useFetch() {
       if (userSession.isLoggedIn) {
         options.headers = {
           ...options.headers,
+          credentials: 'include',
           mode: 'cors',
           Authorization: `Bearer ${userSession.token}`,
         }
